48bfdb58c11a44d7662e2d4ecde45a11cc3e3cd6
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-14 19:31:01 +0200

    Software Engineering

--- src/pages/Courses page/SoftwareEng.js
+++ src/pages/Courses page/SoftwareEng.js
@@ -5,6 +5,7 @@ import Footer from "../../components/Footers/footer";
 import "./AllCourses.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { useNavigate } from 'react-router-dom';
 
 // import holidayPic from "../Courses page/Pictures/holidaysBootcamp.jpg";
 // import SoftwareEngineeringPic from "../Courses page/Pictures/softPic.jpg";
@@ -13,9 +14,12 @@ import "aos/dist/aos.css";
 
 
 const FullStack = () => {
+  const navigate = useNavigate();
+
   useEffect(()=>{
     Aos.init({duration:3000});
   },[])
+
   return (
     <div>
       <Nav />
@@ -51,14 +55,14 @@ const FullStack = () => {
 
           Welcome to our Frontend Development hub! Here, we craft seamless user experiences by bringing web interfaces to life. Using the powerful combination of JavaScript, HTML, and CSS, we transform ideas into interactive and visually appealing websites. Our expertise extends to harnessing the capabilities of ReactJS, ensuring dynamic and efficient web solutions.<span className='hide'> Dive into the world of responsive design and cutting-edge web skills with us – where simplicity meets innovation, and user interfaces come to vibrant life. Elevate your digital presence with our frontend development prowess!</span>
           </p>
-          <button className='speci-card-btn'>Get Started!</button>
+          <button onClick={() => navigate('/februaryintake')} className='speci-card-btn'>Apply Now!</button>
         </div>
         <div className='backend-specialization'>
           <h4>Backend Development</h4>
           <p>
           Backend development is the behind-the-scenes magic that powers websites, making them functional and dynamic. In this process, we leverage a powerful trio of technologies: JavaScript for scripting, Express JS for building robust web applications, and Node JS for executing server-side code efficiently. MongoDB, a flexible and scalable database, helps store and manage data seamlessly.<span className='hide'>Together, these tools enable us to create interactive and responsive web applications, ensuring a smooth and efficient user experience.</span> 
           </p>
-          <button className='speci-card-btn'>Get Started!</button>
+          <button onClick={() => navigate('/februaryintake')} className='speci-card-btn'>Apply Now!</button>
         </div>
       </div>
 
